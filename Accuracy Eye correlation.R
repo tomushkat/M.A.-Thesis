cf7c6fe8866eb8e14cec50dea36e7c913985bc11
tomushkat <62655326+tomushkat@users.noreply.github.com>
2020-07-10 00:35:47 +0300

    Add files via upload

--- Accuracy Eye correlation.R
+++ Accuracy Eye correlation.R
@@ -1,21 +1,28 @@
+accData <- behavioralData %>%
+  mutate(Answer = ifelse(Answer == 1, 'Yes', 'No')) %>%
+  filter(newBlock == 2) %>%
+  filter(ID != 30) %>% filter(ID != 31) %>%
+  group_by(ID, Answer) %>%
+  summarise(meanAccuracy = mean(newIsCorrect, na.rm = TRUE)) %>%
+  mutate(Group = paste0(ID, Answer))
 
-accEyeData <- ddply(B2, c('ID', 'Answer'), summarise,
-                    meanAccuracy = mean(newIsCorrect, na.rm = TRUE))
-accEyeData <- accEyeData[complete.cases(accEyeData), ]
-accEyeData$Answer <- ifelse(accEyeData$Answer == 1, 'Yes', 'No')
-accEyeData$Group <- paste0(accEyeData$ID, accEyeData$Answer)
+accData <- accData[complete.cases(accData), ]
 
-B2EyeData <- B2[B2$TrialTime > 2150 & B2$TrialTime < 2300, ]
-eyeAccData <- ddply(B2EyeData, c('ID', 'Trial.x',  'Answer'), summarise,
-                    Mean = mean(gy, na.rm = TRUE))
-eyeAccData2 <- ddply(eyeAccData, c('ID',  'Answer'), summarise,
-                     MeanEyegaze = mean(Mean, na.rm = TRUE))
-eyeAccData2$Answer <- ifelse(eyeAccData2$Answer == 1, 'Yes', 'No')
-eyeAccData2$Group <- paste0(eyeAccData2$ID, eyeAccData2$Answer)
+B2EyeData <- Data %>%
+  filter(Block == 2) %>%
+  filter(TrialTime > 2150 & TrialTime < 2300) %>%
+  group_by(ID, Trial.x, Answer) %>%
+  summarise(meanEye = mean(gy, na.rm = TRUE)) %>%
+  group_by(ID, Answer) %>%
+  summarise(MeanEyegaze = mean(meanEye, na.rm = TRUE)) %>%
+  mutate(Answer = ifelse(Answer == 1, 'Yes', 'No'),
+         Group  = paste0(ID, Answer)) 
 
-finalData <- merge(accEyeData, eyeAccData2, by = c('Group'))
-finalData$ID.y <- NULL
-finalData$Answer.y <- NULL
+finalData <- merge(accData, B2EyeData, by = c('Group'))
+finalData <- finalData %>%
+  mutate(ID.y = NULL,
+         Answer.y = NULL)
+
 diffAcc <- finalData$meanAccuracy[finalData$Answer.x == 'Yes'] - finalData$meanAccuracy[finalData$Answer.x == 'No']
 diffMeanEyegaze <- finalData$MeanEyegaze[finalData$Answer.x == 'Yes'] - finalData$MeanEyegaze[finalData$Answer.x == 'No']
 
@@ -25,41 +32,54 @@ corData <- data.frame(ID = unique(finalData$ID), yesAcc = finalData$meanAccuracy
 corTable <- apa.cor.table(corData[ , c(2:7)], filename="Table2_APA.doc", table.number=1)
 corData <- round(corData, 2)
 
-corData$directionEye <- as.factor(ifelse(corData$diffMeanEyegaze > 0, TRUE, FALSE))
-ggplot(corData, aes(x = yesAcc, y = noAcc)) + 
-  geom_point(aes(color = YesGaze, size = noGaze, shape = directionEye)) +
-  ggtitle('Accuracy and Eye-gaze correlation') + xlab('Yes accuracy') + ylab('No accuracy') + 
+corData$directionEye1 <- ifelse(corData$diffMeanEyegaze < 0, 'rightDirection', 'wrongDirection')
+
+Title <- c('Accuracy and Eye-gaze differences')
+Name <- c('Accuracy and Eye-gaze differences.pdf')
+corData %>%
+  ggplot(aes(x = diffAcc, y = diffMeanEyegaze)) + 
+  geom_point(aes(shape = directionEye1), size = 3) +
+  #scale_color_manual(breaks = c('rightDirection', 'wrongDirection'), values = c('blue', 'red')) +
+  ggtitle(Title) + xlab('Accuracy: Yes - No') + ylab('ET: Yes - No') +
+  theme_classic() + 
+  theme(panel.grid.major = element_line(color = 'grey')) + 
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(axis.text   = element_text(size = 12),
         axis.title  = element_text(size = 14, face = "bold"),
         axis.line.y = element_line(color = "black", size = 1),
         axis.line.x = element_line(color = "black", size = 1)) +
   theme(axis.line.y = element_line(color = "black", size = 1),
-        axis.line.x = element_line(color = "black", size = 1))   
+        axis.line.x = element_line(color = "black", size = 1))  + 
 geom_label_repel(aes(label = ID),
                  box.padding   = 0.35, 
                  point.padding = 0.5,
-                 segment.color = 'grey50', position=position_dodge(0.8))
-ggsave('Accuracy and Eye-gaze correlation.pdf', width = 24, height = 20, units = "cm")
+                 segment.color = 'grey50', position = position_dodge(0.8))
+ggsave(Name, width = 24, height = 20, units = "cm")
 
 
-corData$sdETDistance <- corData$diffMeanEyegaze / sd(corData$diffMeanEyegaze)
+corData <- corData %>%
+  mutate(sdETDistance = diffMeanEyegaze / sd(diffMeanEyegaze),
+         sdDiffAcc = diffAcc / sd(diffAcc))
 
-corData$sdDiffAcc <- corData$diffAcc / sd(corData$diffAcc)
+Title <- c('SD Accuracy and Eye-gaze differences')
+Name <- c('SD Accuracy and Eye-gaze differences.pdf')
 
-
-ggplot(corData, aes(x = sdDiffAcc, y = sdETDistance)) + 
-  geom_point() +
-  ggtitle('Distance Accuracy difference and Eye-gaze difference correlation 2150-2300ms') + xlab('Accuracy difference') + ylab('Eye gaze difference') + 
+corData %>%
+  ggplot(aes(x = sdDiffAcc, y = sdETDistance)) + 
+  geom_point(aes(shape = directionEye1), size = 3) +
+  #scale_color_manual(breaks = c('rightDirection', 'wrongDirection'), values = c('blue', 'red')) +
+  ggtitle(Title) + xlab('Accuracy: Yes - No') + ylab('ET: Yes - No') +
+  theme_classic() + 
+  theme(panel.grid.major = element_line(color = 'grey')) + 
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(axis.text   = element_text(size = 12),
         axis.title  = element_text(size = 14, face = "bold"),
         axis.line.y = element_line(color = "black", size = 1),
         axis.line.x = element_line(color = "black", size = 1)) +
   theme(axis.line.y = element_line(color = "black", size = 1),
-        axis.line.x = element_line(color = "black", size = 1)) +
+        axis.line.x = element_line(color = "black", size = 1))  
   geom_label_repel(aes(label = ID),
                    box.padding   = 0.35, 
                    point.padding = 0.5,
-                   segment.color = 'grey50', position=position_dodge(0.8))
-ggsave('With names SD distance Accuracy difference and Eye-gaze difference correlation 2150-2300ms.pdf', width = 24, height = 20, units = "cm")
+                   segment.color = 'grey50', position = position_dodge(0.8))
+ggsave(Name, width = 24, height = 20, units = "cm")
